2b5e6775b3e47b69e5952720a341af8f1fb9459f
Fotso-gif <111807337+Fotso-gif@users.noreply.github.com>
2025-04-23 13:25:23 +0100

    Up

--- Trackend_Web/Trackend_Web/urls.py
+++ Trackend_Web/Trackend_Web/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
+    path('dashboard/', include('admin.urls')),
 ]
 
 #CONFIGURATION FOR MEDIA FILES 
